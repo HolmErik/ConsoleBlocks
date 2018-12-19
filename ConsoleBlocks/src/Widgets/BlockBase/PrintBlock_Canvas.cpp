afea5fcdf9274ea7ca7d62f1556f0152d2f7bfc0
Lambipapp <lars.johan.urban.laven@gmail.com>
2018-12-19 14:25:22 +0100

    InputBoxFunction

--- ConsoleBlocks/src/Widgets/BlockBase/PrintBlock_Canvas.cpp
+++ ConsoleBlocks/src/Widgets/BlockBase/PrintBlock_Canvas.cpp
@@ -1,11 +1,19 @@
 #include "PrintBlock_Canvas.h"
 
+
 PrintBlock_Canvas::PrintBlock_Canvas(QWidget *parent)
 	: CBBaseBlock(parent)
 {
 	ui.setupUi(this);
+	inputValue = findChild<QTextEdit*>("TE");
 }
 
 PrintBlock_Canvas::~PrintBlock_Canvas()
 {
+}
+
+std::string PrintBlock_Canvas::GetInputString()
+{
+	std::string code = "CBPrint(\"" + inputValue->toPlainText().toStdString() + "\");";
+	return code;
 }
--- ConsoleBlocks/src/Widgets/BlockBase/PrintBlock_Canvas.h
+++ ConsoleBlocks/src/Widgets/BlockBase/PrintBlock_Canvas.h
@@ -2,6 +2,7 @@
 
 #include <CBBaseBlock.h>
 #include "ui_PrintBlock_Canvas.h"
+#include "QtWidgets/qtextedit.h"
 
 class PrintBlock_Canvas : public CBBaseBlock
 {
@@ -10,7 +11,9 @@ class PrintBlock_Canvas : public CBBaseBlock
 public:
 	PrintBlock_Canvas(QWidget *parent = Q_NULLPTR);
 	~PrintBlock_Canvas();
+	std::string GetInputString();
 
 private:
 	Ui::PrintBlock_Canvas ui;
+	QTextEdit *inputValue;
 };
